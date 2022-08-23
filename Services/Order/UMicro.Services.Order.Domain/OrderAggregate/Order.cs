cb5b39aee013c706fae22587ceb6069f7347772d
Batuhan Türk <batuay34@outlook.com>
2022-08-23 17:56:43 +0300

    Domain,core,infrastructure

---
+++ Services/Order/UMicro.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UMicro.Services.Order.Domain.Core;
+
+namespace UMicro.Services.Order.Domain.OrderAggregate
+{//ef core features
+
+    //owned types
+    //shadow property
+    //backing field
+    public class Order:Entity,IAggregateRoot
+    {
+        public DateTime CreatedDate { get; private set; }
+        public Address Address { get; private set; }
+        public string BuyerId { get; private set; }//userid
+
+        private readonly List<OrderItem> _orderItems;
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order(string buyerId,Address address)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            BuyerId = buyerId;
+            Address = address;
+        }
+
+        public void AddOrderItem(string productId,string productName,decimal price,string pictureUrl)
+        {
+            var existProduct = _orderItems.Any(x => x.ProductId == productId);
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+                    
+            }
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
+    }
+}
