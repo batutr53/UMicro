9127704110e33fef5dea5a63c42cda0484e06402
Batuhan Türk <batuay34@outlook.com>
2022-08-24 11:55:31 +0300

    Order  - FakePayment

---
+++ Services/FakePayment/UMicro.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UMicro.Shared.ControllerBases;
+using UMicro.Shared.Dtos;
+
+namespace UMicro.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+    }
+}
--- Services/FakePayment/UMicro.Services.FakePayment/Program.cs
+++ Services/FakePayment/UMicro.Services.FakePayment/Program.cs
@@ -1,16 +1,12 @@
-using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.EntityFrameworkCore;
 using System.IdentityModel.Tokens.Jwt;
-using UMicro.Services.Order.Infrastructure;
-using UMicro.Shared.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
 ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
+
 var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
 
 JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
@@ -18,19 +14,9 @@ JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
     options.Authority = configuration["IdentityServerURL"];
-    options.Audience = "resource_order";
+    options.Audience = "resource_payment";
     options.RequireHttpsMetadata = false;
 });
-builder.Services.AddDbContext<OrderDbContext>(opt =>
-{
-    opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), configure =>
-    {
-        configure.MigrationsAssembly("UMicro.Services.Order.Infrastructure");
-    });
-});
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<ISharedIdentityService,SharedIdentityService>();
-builder.Services.AddMediatR(typeof(UMicro.Services.Order.Application.Handlers.CreateOrderCommandHandler).Assembly);
 builder.Services.AddControllers(opt =>
 {
     opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
